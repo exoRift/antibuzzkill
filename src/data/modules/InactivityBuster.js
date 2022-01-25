e0d76466589edf77384c7e78a8877dabed4e60c5
Arthur Neuman <arthurbneuman@gmail.com>
2022-01-24 21:03:34 -0500

    Post memes in embeds

--- src/data/modules/InactivityBuster.js
+++ src/data/modules/InactivityBuster.js
@@ -1,7 +1,5 @@
 const fetch = require('node-fetch')
 
-const extRegex = /\.(.+)$/
-
 class InactivityBuster {
   constructor (min, max, client, guild, apiurl) {
     this._min = min
@@ -12,35 +10,49 @@ class InactivityBuster {
 
     this.lastMessageTimestamp = Date.now()
 
-    this.timeout = setTimeout(this.action.bind(this), Math.random() * (max - min) + min)
+    this.timeout = setTimeout(this.action.bind(this), this.getRandInterval())
+  }
+
+  getRandInterval () {
+    return Math.random() * (this._max - this._min) + this._min
   }
 
   refreshTimestamp () {
     this.lastMessageTimestamp = Date.now()
 
     clearTimeout(this.timeout)
-    this.timeout = setTimeout(this.action.bind(this), Math.random() * (this._max - this._min) + this._min)
+    this.timeout = setTimeout(this.action.bind(this), this.getRandInterval())
   }
 
   action () {
-    this.timeout = setTimeout(() => this.action())
+    this.refreshTimestamp()
 
     return fetch(this._apiurl)
       .then((meme) => meme.json())
-      .then((meme) => fetch(meme.url)
-        .then((image) => image.buffer())
-        .then((image) => {
-          const channels = this._client.guilds.get(this._guild).channels.filter((c) => c.permissionsOf(this._client.user.id).has('sendMessages') &&
-            !c.type)
-          const channel = channels[parseInt(Math.random() * (channels.length - 1))]
+      .then((meme) => {
+        const channels = this._client.guilds.get(this._guild).channels.filter((c) => c.permissionsOf(this._client.user.id).has('sendMessages') &&
+          !c.type)
+        const channel = channels[parseInt(Math.random() * (channels.length - 1))]
 
-          if (channel) {
-            channel.createMessage(meme.title, {
-              name: 'meme' + extRegex.exec(meme.url)[1],
-              file: image
-            })
-          } else console.warn('NO CHANNEL FOUND WITH PERMISSIONS')
-        }))
+        if (channel) {
+          return channel.createMessage({
+            embed: {
+              author: {
+                name: 'This server is too silent... Discuss this meme!'
+              },
+              title: meme.title,
+              url: meme.postLink,
+              color: Math.random() * 0xffffff,
+              image: {
+                url: meme.url
+              },
+              footer: {
+                text: `Posted by ${meme.author} in r/${meme.subreddit}`
+              }
+            }
+          })
+        } else console.warn('NO CHANNEL FOUND WITH PERMISSIONS')
+      })
 
       .catch(console.error)
   }
